17f5c5bef30c43da0f24b64669a7de7c0b84513a
Brandon Abela <brandonabela9@gmail.com>
2018-11-26 01:50:01 +0100

    Add Top Navigation and Side Navigation

--- javascript/websiteManager.js
+++ javascript/websiteManager.js
@@ -1,12 +1,31 @@
-// Function is responsible on detecting when the user stores to add the class to navigation bar
+jQuery(document).ready(function ($) {
+    // Responsible for collapsing and expanding the side navigation
 
-$(document).scroll("scroll", function() {
-    if ($(document).scrollTop() > 0)
-    {
-        $(".navigationSection .navbar").addClass("navbarScrolled");
-    }
-    else
-    {
-        $(".navigationSection .navbar").removeClass("navbarScrolled");
-    }
+    $('.menuToggle').on('click', function(event) {
+        let windowWidth = $(window).width();
+
+        if (windowWidth < 1000) {
+            $('body').removeClass('expandSideNavigation');
+
+            if (windowWidth < 750) {
+                $('sideNavigation').slideToggle();
+            }
+            else {
+                $('sideNavigation').toggleClass('open-menu');
+            }
+        }
+        else {
+            $('body').toggleClass('expandSideNavigation');
+            $('sideNavigation').removeClass('open-menu');
+        }
+    });
+
+    // Responsible for adding the headers to the drop down in side navigation collapsed
+
+    $(".menuDropDown").each(function() {
+        $(this).on('click', function() {
+            let $dropDownTitle = $(this).children('.menuDropDownTitle').text();
+            $(this).children('.dropdown-menu').prepend('<li class="subtitle">' + $dropDownTitle + '</li>');
+        });
+    });
 });
