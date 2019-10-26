579238533dad0657daf4e96ed4b07ef21e4b73f9
Brandon Abela <brandonabela9@gmail.com>
2019-10-26 10:09:12 +0200

    Fix google analytics navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
+declare var gtag: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent {
+  constructor(router: Router) {
+    const navEndEvents = router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+    );
+
+    navEndEvents.subscribe((event: NavigationEnd) => {
+      // Skip recording GA events to our account if in development.
+      if (document.location.hostname !== 'localhost') {
+        gtag('config', 'UA-149941240-1', {
+          page_path: event.urlAfterRedirects
+        });
+      }
+    });
+  }
+}
