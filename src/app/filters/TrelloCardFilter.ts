a636555ab79be9b98fec718af82b2d7a0877836c
Brandon Abela <brandonabela9@gmail.com>
2020-02-27 18:40:50 +0100

    Add more filters to Trello overview page

--- src/app/filters/TrelloCardFilter.ts
+++ src/app/filters/TrelloCardFilter.ts
@@ -6,11 +6,27 @@ import { TrelloCard } from '../models/trello/trello-card';
   pure: false
 })
 export class TrelloCardFilter implements PipeTransform {
-  transform(trelloCards: TrelloCard[], filter: string): any {
-    if (!trelloCards || !filter) {
+  transform(trelloCards: TrelloCard[], args: string[]): any {
+    const argKeys = Object.keys(args);
+
+    const filterKey = argKeys[0];
+    const columnKey = argKeys[1];
+
+    let filter = args[filterKey];
+    const columns = args[columnKey];
+
+    if (!trelloCards || !filter || !columns) {
       return trelloCards;
     }
 
-    return trelloCards.filter(item => item.cardName.toLowerCase().indexOf(filter.toLowerCase()) >= 0);
+    filter = filter.toLowerCase();
+
+    return trelloCards.filter(item =>
+      (columns[1] && item.cardBoardName.toLowerCase().indexOf(filter) >= 0) ||
+      (columns[2] && item.cardName.toLowerCase().indexOf(filter) >= 0) ||
+      (columns[3] && item.cardDescription.toLowerCase().indexOf(filter) >= 0) ||
+      (columns[4] && item.cardLabels.map(label => label.labelName.toLowerCase()).filter(label => label.indexOf(filter) >= 0).length !== 0) ||
+      (columns[5] && (' ' + item.cardPercentage.toString() + '%').indexOf(filter) >= 0)
+    );
   }
 }
